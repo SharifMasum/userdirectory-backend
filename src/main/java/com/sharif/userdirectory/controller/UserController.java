98eadfcf062164aa9dd57d51eb37d881bd7ceaa9
Shariful Islam <shariful.islam.mec@gmail.com>
2024-03-09 16:04:09 +0200

    Revert "deployment url commit"
    This reverts commit e02ae4a24ac22e307297ff6072d87fa1f047d8d3.

--- src/main/java/com/sharif/userdirectory/controller/UserController.java
+++ src/main/java/com/sharif/userdirectory/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("https://userdirectory.vercel.app")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
